02c29b9f8860feca2ee3834cf2c638bc34887c57
Dorian della Faille <dorian.della.faille@gmail.com>
2021-11-12 12:15:17 +0100

    app works

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,7 @@
 class List < ApplicationRecord
-  validates :name, uniqueness: true
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
+  has_many :reviews
+
+  validates :name, uniqueness: true, presence: true
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
-  validates :title, uniqueness: true
+  has_many :bookmarks
 
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
 end
